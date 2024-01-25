984d6acd6a400694cae40de8b909fcb9813adef9
Luwam Ghebremicael <luwammikael@gmail.com>
2024-01-24 21:22:53 -0800

    deletes unnecessary '/' after a url in the CORS_ALLOWED_ORIGINS =[] inside settings.py

--- beesknees/settings.py
+++ beesknees/settings.py
@@ -147,6 +147,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://kneesrehab.netlify.app/",
+    "https://kneesrehab.netlify.app",
 ]
 
